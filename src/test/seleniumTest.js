505f802dc112aebb1dd0b1b9de7862dd8c2a6526
국남규 <3gguild@naver.com>
2018-01-25 01:44:56 +0900

    remote 환경 구성

--- src/test/seleniumTest.js
+++ src/test/seleniumTest.js
@@ -1,7 +1,8 @@
-require('chromedriver');
+const browserName = 'chrome';   // choose 'chrome' or 'firefox'
 const { Builder, By, Key, until } = require('selenium-webdriver');
-
-const driver = new Builder().forBrowser('chrome').build();
+const driver = new Builder().forBrowser(browserName)
+                            .usingServer('http://192.168.0.7:4444/wd/hub')
+                            .build();
 
 try {
     driver.get('http://www.google.com');
@@ -13,10 +14,19 @@ try {
     // driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN); 와 동일
     const element = driver.findElement(By.name('q'));
     element.sendKeys('webdriver');
-    element.submit();
+
+    let sumbitForm;
+
+    if (browserName === 'chrome') {
+        sumbitForm = driver.findElement(By.name('f'));
+        sumbitForm.submit();
+    } else if (browserName === 'firefox') {
+        sumbitForm = driver.findElement(By.name('btnK'));
+        sumbitForm.click();
+    }
 
     // 10초 대기
-    driver.wait(until.titleMatches(/^asdf/g), 10000);
+    driver.wait(until.titleMatches(/^webdriver/g), 10000);
 } finally {
     driver.getTitle().then(title => {
         console.log(`[Next] Page title is: ${title}`);
