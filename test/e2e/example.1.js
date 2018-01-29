9280e8863ada3d88e0f659d8964cc1e05cad71e7
국남규 <3gguild@naver.com>
2018-01-29 16:37:29 +0900

    then 구문 사용한 예제로 수정

--- test/e2e/example.1.js
+++ test/e2e/example.1.js
@@ -2,26 +2,28 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const driver = new Builder().forBrowser('chrome')
                             .build();
  
-driver.get('http://www.google.com');
-
-driver.getTitle().then(title => {
-    console.log(`::::::[Prev] Page title is: ${title}`);
-});
-
-// driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN); 와 동일
-const element = driver.findElement(By.name('q'));
-element.sendKeys('webdriver');
-
-let sumbitForm;
-
-sumbitForm = driver.findElement(By.name('f'));
-sumbitForm.submit();
-
-// 10초 대기
-driver.wait(until.titleMatches(/^weriver/g), 10000);
-
-driver.getTitle().then(title => {
-    console.log(`::::::[Next] Page title is: ${title}`);
-});
-
-driver.quit();
+driver.get('http://www.google.com')
+    .then(_ => 
+        driver.getTitle().then(title => {
+            console.log(`::::::[Prev] Page title is: ${title}`);
+        })
+    )
+    .then(_ =>
+        // driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN); 와 동일
+        driver.findElement(By.name('q')).sendKeys('webdriver')
+    )
+    .then(_ =>
+        driver.findElement(By.name('f')).submit()
+    )
+    .then(_ =>
+        // 10초 대기
+        driver.wait(until.titleMatches(/^webdriver/g), 10000)
+    )
+    .then(_ =>
+        driver.getTitle().then(title => {
+            console.log(`::::::[Next] Page title is: ${title}`);
+        })
+    )
+    .then(_ =>
+        driver.quit()
+    );
--- test/e2e/example.2.js
+++ test/e2e/example.2.js
@@ -1,28 +1,30 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const driver = new Builder().forBrowser('chrome')
-                            .usingServer('http://10.5.220.163:4444/wd/hub')   // remote 사용 시 hub 도메인으로 작성
+                            .usingServer('http://localhost:4444/wd/hub')   // remote 사용 시 hub 도메인으로 작성
                             .build();
  
-driver.get('http://www.google.com');
-
-driver.getTitle().then(title => {
-    console.log(`::::::[Prev] Page title is: ${title}`);
-});
-
-// driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN); 와 동일
-const element = driver.findElement(By.name('q'));
-element.sendKeys('webdriver');
-
-let sumbitForm;
-
-sumbitForm = driver.findElement(By.name('f'));
-sumbitForm.submit();
-
-// 10초 대기
-driver.wait(until.titleMatches(/^webdriver/g), 10000);
-
-driver.getTitle().then(title => {
-    console.log(`::::::[Next] Page title is: ${title}`);
-});
-
-driver.quit();
+driver.get('http://www.google.com')
+    .then(_ => 
+        driver.getTitle().then(title => {
+            console.log(`::::::[Prev] Page title is: ${title}`);
+        })
+    )
+    .then(_ =>
+        // driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN); 와 동일
+        driver.findElement(By.name('q')).sendKeys('webdriver')
+    )
+    .then(_ =>
+        driver.findElement(By.name('f')).submit()
+    )
+    .then(_ =>
+        // 10초 대기
+        driver.wait(until.titleMatches(/^webdriver/g), 10000)
+    )
+    .then(_ =>
+        driver.getTitle().then(title => {
+            console.log(`::::::[Next] Page title is: ${title}`);
+        })
+    )
+    .then(_ =>
+        driver.quit()
+    );
